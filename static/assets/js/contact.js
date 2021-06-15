ecbc909ee41ae321b03ad2e1ff9cf594b285c6ad
eric mutua <erotush77@gmail.com>
2021-06-15 11:16:59 +0300

    Modify: section_form.html, contact.js

--- static/assets/js/contact.js
+++ static/assets/js/contact.js
@@ -1,3 +1,15 @@
-const onSubmit = () => {
-    console.log("Message Submitted")
+const onSubmit = (e) => {
+   
+    e.preventDefault();
+    const formData = {}
+
+    formData['name'] = document.name.target.value
+    formData['email'] = document.email.target.value
+    formData['subject'] = document.subject.target.value
+    formData['message'] = document.message.target.value
+    formData['consent'] = document.message.target.value
+
+    console.log(formData)
+
+    document.contactForm.action = "/thank-you"
 }
